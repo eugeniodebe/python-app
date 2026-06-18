5aadec0d210d98f29b29534aca810302d5cad817
Eugenio <eugenio.debenedetti@fabdata.io>
2026-06-18 11:38:03 +0100

    Change message again - test fully automatic loop

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human! <3 - deployed via CI/CD!',
+        'message': 'You are doing great, little human! <3 - fully automated GitOps!',
         'deployed_on': 'kubernetes'
     })
 
